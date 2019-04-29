7f40d0841a8f7db1d40eab576157c6015f29f81e
Paroon <paroonk@hotmail.com>
2019-04-29 23:00:41 +0700

    IS

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\SCG\PycharmProjects\DCA\venv"
+$env:VIRTUAL_ENV="C:\Users\Paroon\PycharmProjects\DCA\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
